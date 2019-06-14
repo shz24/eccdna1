72c86e326eb33a57ffd8f527fade05508865ec59
szhanghdp <shz24@cornell.edu>
2019-06-14 12:08:15 -0400

    added src

---
+++ src/_config.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+import numpy as np
+
+PRJ_DIR="/cluster/shz24/prj/eccdna1"
+DATA_DIR= os.path.join(PRJ_DIR,"data")
+SRC_DIR= os.path.join(PRJ_DIR,"src")
+OUT_DIR= os.path.join(PRJ_DIR,"out")
