a37fe4710750100a6c6b5eac12598204354e7540
szhanghdp <shz24@cornell.edu>
2019-06-18 15:10:44 -0400

    modified _config to fix typo

--- src/_config.py
+++ src/_config.py
@@ -5,4 +5,4 @@ import numpy as np
 PRJ_DIR="/cluster/shz24/prj/eccdna1"
 DATA_DIR= os.path.join(PRJ_DIR,"data")
 SRC_DIR= os.path.join(PRJ_DIR,"src")
-OUT_DIR= os.path.join(PRJ_DIR,"out")
+OUT_PLACE= os.path.join(PRJ_DIR,"out")
