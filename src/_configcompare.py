1dc24a1319582259a6a7d27e5931a8718aa123e0
szhanghdp <shz24@cornell.edu>
2019-06-18 15:08:16 -0400

    created configpatterns, configcompare, ipynb file for figure creation

---
+++ src/_configcompare.py
@@ -0,0 +1,12 @@
+CONFIGURATION_NAME = 'Shibata Comparison'
+
+COMPARISON_FILES = [
+    {'name':'Embryo Heart',
+    'filename': 'converted_mm10_heart.bed'},
+    {'name': 'Embryo Liver',
+    'filename': 'converted_mm10_liver.bed'},
+    {'name': 'Embryo Brain',
+    'filename': 'converted_mm10_brain.bed'},
+]
+
+
---
+++ src/_configpatterns.py
@@ -0,0 +1,28 @@
+CONFIGURATION_NAME = 'default'
+
+REGRESSION_TRACK_FILES = [
+    {'name':'Mouse Cell Expression',
+    'filename': 'mm10_cellexpression.csv'},
+    {'name':'Mouse Simple Repeats',
+    'filename': 'mm10_simplerepeats.csv'},
+    {'name':'Mouse CpGIslands',
+    'filename': 'mm10_cpgislands.csv'},
+]
+
+ALL_TRACK_FILES = [
+    {'name':'Mouse Cell Expression',
+    'filename': 'mm10_cellexpression.csv'},
+    {'name':'Mouse Interrupted Repeats',
+    'filename': 'mm10_interruptrepeats.csv'},
+    {'name':'Mouse Segmental Duplication',
+    'filename': 'mm10_segmentaldups.csv'},
+    {'name':'Mouse CpGIslands',
+    'filename': 'mm10_cpgislands.csv'},
+    {'name':'Mouse Microsatellites',
+    'filename': 'mm10_microsatellites.csv'},
+    {'name':'Mouse Simple Repeats',
+    'filename': 'mm10_simplerepeats.csv'},
+   
+]
+
+
